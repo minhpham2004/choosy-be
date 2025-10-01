1a3db6f834c8f288b026150164d52730ad5443ed
HarrySolterbeck <harrysolterbeck@gmail.com>
2025-10-02 02:03:32 +1000

    jest changes

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,16 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/*.spec.ts'],
+  roots: ['<rootDir>/src', '<rootDir>/test'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
   moduleFileExtensions: ['ts', 'js', 'json'],
-  // keep tests pure-unit: do not import AppModule or start Nest app here
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'cobertura'],
-  // Optional: ignore generated code
-  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
+  // 👇 tell Jest how to resolve `src/...`
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverageFrom: ['src/**/*.ts', '!src/main.ts'],
+  coverageReporters: ['text', 'cobertura'],
+  testPathIgnorePatterns: ['/dist/'],
 };
