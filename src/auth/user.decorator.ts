165bf702a603b5158dadb2a8e6c27486f646f8d5
Minh Pham <ngocminhpham2004hn@gmail.com>
2025-10-27 12:23:54 +1100

    update comments

--- src/auth/user.decorator.ts
+++ src/auth/user.decorator.ts
@@ -1,6 +1,7 @@
-// Minh Pham, Nathan Ravasini
+// Minh Pham
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+// Custom decorator to easily extract user info from the request (e.g., from JWT)
 export const GetUserInfo = createParamDecorator(
   (data: keyof any, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
